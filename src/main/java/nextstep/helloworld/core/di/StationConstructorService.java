872cd0276b3aa2675b5fe9c21f874923351bbfa3
hectick <ch.yeondev@gmail.com>
2023-04-12 17:45:41 +0900

    수정

--- src/main/java/nextstep/helloworld/core/di/StationConstructorService.java
+++ src/main/java/nextstep/helloworld/core/di/StationConstructorService.java
@@ -1,14 +1,12 @@
 package nextstep.helloworld.core.di;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StationConstructorService {
 
-    private StationRepository stationRepository;
+    private final StationRepository stationRepository;
 
-    @Autowired
     public StationConstructorService(StationRepository stationRepository) {
         this.stationRepository = stationRepository;
     }
