16926397e32e66da2ab2e1110165944d28bfd150
hectick <ch.yeondev@gmail.com>
2023-04-12 17:21:05 +0900

    di 학습

--- src/main/java/nextstep/helloworld/core/di/StationConstructorService.java
+++ src/main/java/nextstep/helloworld/core/di/StationConstructorService.java
@@ -1,7 +1,17 @@
 package nextstep.helloworld.core.di;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class StationConstructorService {
+
     private StationRepository stationRepository;
+
+    @Autowired
+    public StationConstructorService(StationRepository stationRepository) {
+        this.stationRepository = stationRepository;
+    }
 
     public String sayHi() {
         return stationRepository.sayHi();
